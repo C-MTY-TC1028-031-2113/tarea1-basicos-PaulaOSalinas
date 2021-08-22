36270ccc2d7943d04fbbfee7a9a3325bfa2eb94d
PaulaOSalinas <a01198180@itesm.mx>
2021-08-22 18:43:34 -0500

    22082021

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,10 +1,22 @@
 def main():
     from math import sqrt
     pass
+
+oper1 = (Fraction(11, 4)) + (Fraction (14, 3)) - (Fraction (16, 5)) + (Fraction (11, 2))
+    oper2 = 2 * math.sqrt(35**2) + 4 * math.sqrt(36**3) - 6 * math.sqrt(49**2)
+    a = 4
+    b= 5
+    oper3 = (a**3 + 2 * b**2)/(4*a)
+    oper4 = (2*(a+b)**2 + 4*(a-b)**2)/(a*b**2)
+    oper5 = math.sqrt((a+b)**2 + 2**(a+b))/((2*a + 2*b)**2)
+    print(float(oper1))
+    print(float(oper2))
+    print(float(oper3))
+    print(float(oper4))
+    print(float(oper5))
     
    
-
-
+   
 
 if __name__ == '__main__':
     main()
